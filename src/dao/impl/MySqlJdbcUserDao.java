5e2b7507cf4eac1a88efa6e0107943e44b8bba3a
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-07-20 11:36:10 +0200

    Fixed save user sql error

--- src/dao/impl/MySqlJdbcUserDao.java
+++ src/dao/impl/MySqlJdbcUserDao.java
@@ -19,30 +19,30 @@ public class MySqlJdbcUserDao implements UserDao{
 
 	@Override
 	public boolean saveUser(UserDto user) {
-		try (var conn = DBUtils.getConnection();
-				var ps = conn.prepareStatement("INSERT INTO user (first_name, last_name, email, fk_user_role, " + 
-								"money, credit_card, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?);")){
-			ps.setString(1, user.getFirstName());
-			ps.setString(2, user.getLastName());
-			ps.setString(3, user.getEmail());
-			if (user.getRoleDto() != null && user.getRoleDto().getId() != null) {
-				ps.setInt(4, user.getRoleDto().getId());
-			}else {
-				ps.setNull(4, java.sql.Types.NULL);
-			}
-			ps.setBigDecimal(5, user.getMoney());
-			ps.setString(6, user.getCreditCard());
-			ps.setString(7, user.getPassword());
-			
-			ps.executeUpdate();
-			return true;
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-		
+	    try (var conn = DBUtils.getConnection();
+	         var ps = conn.prepareStatement("INSERT INTO user (first_name, last_name, email, password, " +
+	                 "fk_user_role, money, credit_card) VALUES (?, ?, ?, ?, ?, ?, ?);")) {
+	        ps.setString(1, user.getFirstName());
+	        ps.setString(2, user.getLastName());
+	        ps.setString(3, user.getEmail());
+	        ps.setString(4, user.getPassword());
+	        if (user.getRoleDto() != null && user.getRoleDto().getId() != null) {
+	            ps.setInt(5, user.getRoleDto().getId());
+	        } else {
+	            ps.setNull(5, java.sql.Types.INTEGER);
+	        }
+	        ps.setBigDecimal(6, user.getMoney());
+	        ps.setString(7, user.getCreditCard());
+
+	        ps.executeUpdate();
+	        return true;
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
 	}
+
 
 	@Override
 	public List<UserDto> getUsers() {
